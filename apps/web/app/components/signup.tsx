dac4c4da39efe12a0b5e690cd66177a508bfabe5
kshaan-ali <kshaaneali@gmail.com>
2024-08-18 21:57:18 +0530

    landing page

--- apps/web/app/components/signup.tsx
+++ apps/web/app/components/signup.tsx
@@ -1,10 +1,13 @@
 "use client"
 import { getServerSession } from "next-auth"
 import { signIn, signOut, useSession } from "next-auth/react"
-
+import { useRouter } from 'next/navigation'
 import { Button } from "@repo/ui/components/ui/button"
 
 
 export function Signup(){
-    return <Button  variant="outline">Sign Up</Button>
+    const router = useRouter()
+    return <Button onClick={function(){
+        router.push('/signup')
+    } } variant="outline">Sign Up</Button>
 }
